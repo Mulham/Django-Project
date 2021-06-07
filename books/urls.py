c0e86081cccaf9d8a27d2fb30a5ace5a35cd213e
mulham <mulham94@gmail.com>
2021-06-07 16:24:13 +0300

    comment2

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
 
     #comment
-
+    #comment2
     path("", views.index, name="index"),
     path("<int:pk>", views.book_detail, name="book_detail"),
     path("create/", views.model_form_upload, name="create"),
